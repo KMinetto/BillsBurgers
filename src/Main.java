fe79ee9613afce8c4b859a57d118147c5ca192dc
Kilian MINETTO <killian.minetto@gmail.com>
2022-07-04 22:00:34 +0200

    Creating HealthyBurger and DeluxeBurger class

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
 import hamburger.Hamburger;
+import hamburger.deluxe.DeluxeBurger;
+import hamburger.healthy.HealthyBurger;
 
 public class Main {
   public static void main(String[] args) {
@@ -9,5 +11,15 @@ public class Main {
     hamburger.addHamburgerAddition3("Cheese", 1.12);
     hamburger.addHamburgerAddition4("Carrot", 1.26);
     System.out.println(hamburger.itemizeHamburger() + " €");
+
+    HealthyBurger healthy = new HealthyBurger("Bacon", 5.67);
+    healthy.addHamburgerAddition1("Egg", 5.43);
+    healthy.addHealthAddition1("Salad", 0.64);
+    healthy.addHealthAddition2("Tomato", 0.27);
+    System.out.println(healthy.itemizeHamburger() + " €");
+
+    DeluxeBurger deluxe = new DeluxeBurger();
+    deluxe.addHamburgerAddition1("Tomato", 500.3);
+    System.out.println(String.format("%.2f", deluxe.itemizeHamburger()));
   }
 }
